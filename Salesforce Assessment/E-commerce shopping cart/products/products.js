f358d8566d25cf22829273ba40d1eaab182a0895
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-11-20 11:46:38 -0700

    Update products.js

--- Salesforce Assessment/E-commerce shopping cart/products/products.js
+++ Salesforce Assessment/E-commerce shopping cart/products/products.js
@@ -3,8 +3,7 @@
 $(document).ready(function() {
     $(document).on('click','.more-info-link', function(event){
       event.preventDefault();
-      //$(this).parent().find('.more-info').toggle('fast');
-      //$(this).parent().find('.more-info').fadeToggle('slow');
+      
       $(this).parent().find('.more-info').slideToggle('slow');
       $(this).animate({"opacity": 0.5, "margin-left": 10}, 'fast').animate({"opacity": 1.0, "margin-left": 0}, 'fast');
     })
@@ -14,12 +13,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name = $(this).parent().children().first().data('name');
       var data = $("#data").val();
       let price = $(this).parent().children().first().next().next().data('price');
       let value = price * data;
-      // console.log(pic, name, data, value);
+      
       
       localStorage.setItem('pic', pic);
       localStorage.setItem('name', name);
@@ -35,12 +34,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic1 = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name1 = $(this).parent().children().first().data('name');
       var data1 = $("#data").val();
       let price1 = $(this).parent().children().first().next().next().data('price');
       let value1 = price1 * data1;
-      // console.log(pic, name, data, value);
+      ;
       
       localStorage.setItem('pic1', pic1);
       localStorage.setItem('name1', name1);
@@ -56,12 +55,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic2 = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name2 = $(this).parent().children().first().data('name');
       var data2 = $("#data").val();
       let price2 = $(this).parent().children().first().next().next().data('price');
       let value2 = price2 * data2;
-      // console.log(pic, name, data, value);
+      ;
       
       localStorage.setItem('pic2', pic2);
       localStorage.setItem('name2', name2);
@@ -77,12 +76,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic3 = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name3 = $(this).parent().children().first().data('name');
       var data3 = $("#data").val();
       let price3 = $(this).parent().children().first().next().next().data('price');
       let value3 = price3 * data3;
-      // console.log(pic, name, data, value);
+      ;
       
       localStorage.setItem('pic3', pic3);
       localStorage.setItem('name3', name3);
@@ -96,12 +95,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic4 = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name4 = $(this).parent().children().first().data('name');
       var data4 = $("#data").val();
       let price4 = $(this).parent().children().first().next().next().data('price');
       let value4 = price4 * data4;
-      // console.log(pic, name, data, value);
+      ;
       
       localStorage.setItem('pic4', pic4);
       localStorage.setItem('name4', name4);
@@ -115,12 +114,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic5 = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name5 = $(this).parent().children().first().data('name');
       var data5 = $("#data").val();
       let price5 = $(this).parent().children().first().next().next().data('price');
       let value5 = price5 * data5;
-      // console.log(pic, name, data, value);
+      ;
       
       localStorage.setItem('pic5', pic5);
       localStorage.setItem('name5', name5);
@@ -134,12 +133,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic6 = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name6 = $(this).parent().children().first().data('name');
       var data6 = $("#data").val();
       let price6 = $(this).parent().children().first().next().next().data('price');
       let value6 = price6 * data6;
-      // console.log(pic, name, data, value);
+      ;
       
       localStorage.setItem('pic6', pic6);
       localStorage.setItem('name6', name6);
@@ -153,12 +152,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic7 = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name7 = $(this).parent().children().first().data('name');
       var data7 = $("#data").val();
       let price7 = $(this).parent().children().first().next().next().data('price');
       let value7 = price7 * data7;
-      // console.log(pic, name, data, value);
+      ;
       
       localStorage.setItem('pic7', pic7);
       localStorage.setItem('name7', name7);
@@ -172,12 +171,12 @@ $(document).ready(function() {
       
       let pics = $(this).parent().data('pic');
       let pic8 = `<img src=`+'"'+pics+'"'+`></img>`;
-      // let pic = 'a';
+      
       var name8 = $(this).parent().children().first().data('name');
       var data8 = $("#data").val();
       let price8 = $(this).parent().children().first().next().next().data('price');
       let value8 = price8 * data8;
-      // console.log(pic, name, data, value);
+      ;
       
       localStorage.setItem('pic8', pic8);
       localStorage.setItem('name8', name8);
@@ -189,69 +188,9 @@ $(document).ready(function() {
 
     
 
-    // $('body').on('click', '.item-add', function(){
-    //   let pic = $(this).parent().data('pic');
-    //   // let id = $(this).parent().children().first().data('name');
-    //   let name = $(this).parent().children().first().data('name');
-    //   let price = $(this).parent().children().first().next().next().data('price');
-    //   // let pic = $(this).parent().siblings().children().children().first().data('pic');
-    //   let value = price;
-    //   console.log(name, price, pic);
-    //   // console.log(id)
-
-    //   let html= '';
-    //   html += '<div class="item" data-id="id">';
-    //   html += '<div class="name">'+name+'</div>';
-    //   html += `<img src=`+'"'+pic+'"'+`></img>`;
-    //   html += '<div class="description">description</div>';
-    //   html += '<div class="price">'+'GHC '+ price +'</div>';
-    //   html += '<label for="product-quantity-select">Quantity</label>';
-    //   html += '<input type="number" id="product-quantity-select" name="products[0395][quantity]" size="3" min="0" value="1">';
-     
-
-    //   localStorage.setItem((name, value),(pic));
-    //   // $('.cash').prepend(html);
-    //   console.log(localStorage);
-      
-      
-    // });
+    
 })
 
-// function SaveItem() {
-//   alert('saved')
-//   let pic = $(this).parent().data('pic');
-//   var name = $(this).parent().children().first().data('name');
-//   var data = $("#data").val();
-//   let price = $(this).parent().children().first().next().next().data('price');
-//   let value = price;
-//   // console.log(pic, name, data, value);
-//   localStorage.setItem(pic, name, data, value);
-//   doShowAll();
-// }
-
-
-// $('body').on('click', '.item-add', function(){
-//   var name = $(".name").val();
-//   var data = $("#data").val();
-//   console.log(name, data);
-//   localStorage.setItem(name, data);
-//   doShowAll();
-// });
-
-
-
-
-
-
-
-
-// var list = "<tr><th>Item</th><th>Value</th></tr>\n";
-
-// $(window).load(function() {
-//   doShowAll();
-// });
-
-// window.load=doShowAll();
 
 function CheckBrowser() {
   if ('localStorage' in window && window['localStorage'] !== null) {
@@ -264,12 +203,11 @@ function CheckBrowser() {
 
 function doShowAll() {
   if (CheckBrowser()) {
-      // var key = "";
+      
       var list = "<tr><th>Item</th><th>Name</th><th>Quantity</th><th>Value</th></tr>\n";
       
       var i = 0;
-      //For a more advanced feature, you can set a cap on max items in the cart.
-      // for (i = 0; i <= localStorage.length-1; i++) {
+      
           var pic = localStorage.getItem('pic');
           var name = localStorage.getItem('name');
           var quantity = localStorage.getItem('data');
@@ -318,9 +256,8 @@ function doShowAll() {
 
           // list += "<tr><td>" + pic + "</td>\n<td>" + localStorage.getItem(key) + "</td></tr>\n";
           list += "<tr><td>" + pic + "</td>\n<td>" + name + "</td>\n<td>" + quantity + "</td>\n<td>" + value +"</td></tr>\n";
-          // console.log(key,data)
-      // }
-      //If no item exists in the cart.
+          
+      
       if (list == "<tr><th>Item</th><th>Name</th><th>Quantity</th><th>Value</th></tr>\n") {
           list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td>\n<td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
       }
@@ -358,9 +295,7 @@ function doShowAll() {
         }
       }
       $('#cart-total').html(total.toFixed(2));
-      // console.log(total);
-      //Bind the data to HTML table.
-      //You can use jQuery, too.
+      
       document.getElementById('list').innerHTML= list;
   } else {
       alert('Cannot save shopping list as your browser does not support HTML 5');
@@ -391,49 +326,6 @@ function doShowAll() {
   })
 
 }
-
-// function SaveItem() {
-//   var name = $("#name").val();
-//   var data = $("#data").val();
-//   localStorage.setItem(name, data);
-//   doShowAll();
-// }
-
-// function ModifyItem() {
-//   let pics = $(this).parent().data('pic');
-//   let pic1= `<img src=`+'"'+pics+'"'+`></img>`;
-//   var name1 = $(this).parent().children().first().data('name');
-//   var data1 = $("#data").val();
-//   let price1 = $(this).parent().children().first().next().next().data('price');
-//   let value1 = price1 * data1;
-//   //Check if name already exists.
-// //Check if key exists.
-//        if (localStorage.getItem('name1') ==null)
-//        {
-//          //update
-//         localStorage.setItem('pic1', pic1);
-//         localStorage.setItem('name1', name1);
-//         localStorage.setItem('data1', data1);
-//         localStorage.setItem('value1', value1);
-//         //  var new_info=localStorage.getItem(name1);
-//         //  $("#data").val(new_info);
-//         var pic = localStorage.getItem('pic1');
-//         var name = localStorage.getItem('name1');
-//         var quantity = localStorage.getItem('data1');
-//         var value = localStorage.getItem('value1');
-//         list += "<tr><td>" + pic + "</td>\n<td>" + name + "</td>\n<td>" + quantity + "</td>\n<td>" + value +"</td></tr>\n";
-//         document.getElementById('list').innerHTML= list
-//        }
-
-//   doShowAll();
-// }
-
-
-
-
-
-
-
 
 
 function RemoveItem() {
